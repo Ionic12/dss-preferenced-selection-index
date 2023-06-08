4b253c47f778890c40ed42ee7d191e16294046cb
Ionic12 <bagusiqshan81@gmail.com>
2023-06-08 19:18:17 +0700

    Update Latest

--- resources/views/layouts/navbars/guest/navbar.blade.php
+++ resources/views/layouts/navbars/guest/navbar.blade.php
@@ -2,8 +2,8 @@
     <div class="row">
         <div class="col-12">
             <!-- Navbar -->
-            <nav
-                class="navbar navbar-expand-lg blur border-radius-lg top-0 z-index-3 shadow position-absolute mt-4 py-2 start-0 end-0 mx-4">
+            <nav style="margin-right: 0.9%;margin-left: 0.9%"
+                class="navbar navbar-expand-lg blur border-radius-lg top-0 z-index-3 shadow position-absolute mt-4 py-2 start-0 end-0">
                 <div class="container-fluid">
                     <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="/landing#hero">
                         PSIwizard
